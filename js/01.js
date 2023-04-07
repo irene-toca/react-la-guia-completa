0a2cc9b2a93b0db8a2a24d053d32aff89124fdc7
Irene Toca <irene.devops@gmail.com>
2023-04-07 22:07:25 +0100

    Initial commit

---
+++ js/01.js
@@ -0,0 +1,17 @@
+// variables - let const
+
+let cliente = "Juan"
+let precioDescuento = 220
+
+//Re asignar
+cliente = 20 
+cliente = true
+
+//Pueden iniciar si valor 
+let precio 
+precio = 1000
+
+console.log(precio)
+
+console.log(cliente)
+console.log(precioDescuento)
